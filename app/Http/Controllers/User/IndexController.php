c1153b9735c293168b534d6ab11079ccc8226b80
union ojeaga <unionolivia@unionolivia@gmail.com>
2020-02-27 13:30:37 +0100

    updated views

--- app/Http/Controllers/User/IndexController.php
+++ app/Http/Controllers/User/IndexController.php
@@ -17,14 +17,14 @@ class IndexController extends Controller
      */
     public function index()
     {
-        $staffs = DB::table('users as u')
+        $users = DB::table('users as u')
            ->leftjoin('role_user as ru', 'u.id', '=', 'ru.user_id')
            ->leftjoin('roles as r', 'r.id', '=', 'ru.role_id')
             ->select('u.*', 'r.name as role', 'ru.role_id as roleid')
             ->get();
             
              $roles = Role::all();  
-        return view('user.index', compact('staffs', 'roles'));
+        return view('user.index', compact('users', 'roles'));
     }
 
     /**
@@ -35,11 +35,8 @@ class IndexController extends Controller
     public function create()
     {
         $roles = Role::all();
-        $areas = Area::all();
-        $groups = Group::all();
-        $zones = Zone::all();
         
-        return view('user.create', compact('roles', 'areas', 'groups', 'zones'));
+        return view('user.create', compact('roles'));
     }
 
     /**
@@ -73,9 +70,6 @@ class IndexController extends Controller
         	$user->name = $request->username;
         	$user->lastname = $request->lastname;
         	$user->firstname = $request->firstname;
-        	$user->area_id = $request->area;
-        	$user->group_id = $request->group;
-        	$user->zone_id = $request->zone;
         	$user->gender = $request->gender;
         	$user->address = $request->address;
         	$user->firstname = $request->firstname;
@@ -91,7 +85,7 @@ class IndexController extends Controller
           $user->attachRole($role->id);
 
         	
-          return redirect()->route('staff.index')->with('status', $request->firstname.' created as staff');
+          return redirect()->route('user.index')->with('status', $request->firstname.' created as user');
     }
 
     /**
@@ -103,21 +97,15 @@ class IndexController extends Controller
     public function show($id)
     {
     
-            $staff = DB::table('users as u')
+            $user = DB::table('users as u')
             ->where('u.id', '=', $id)
-           ->leftjoin('areas as a', 'u.area_id', '=', 'a.id')
-           ->leftjoin('groups as g', 'u.group_id', '=', 'g.id')
-           ->leftjoin('zones as z', 'u.zone_id', '=', 'z.id')
            ->leftjoin('role_user as ru', 'u.id', '=', 'ru.user_id')
            ->leftjoin('roles as r', 'r.id', '=', 'ru.role_id')
-            ->select('u.*', 'r.name as role', 'ru.role_id as roleid', 'a.name as area', 'g.name as group', 'z.name as zone')
+            ->select('u.*', 'r.name as role', 'ru.role_id as roleid')
             ->first();
             $roles = Role::all();
-            $areas = Area::all();
-            $groups = Group::all();
-            $zones = Zone::all();
             
-        return view('user.show', compact('staff', 'areas', 'groups', 'zones', 'roles'));
+        return view('user.show', compact('user', 'roles'));
     }
 
     /**
@@ -167,10 +155,10 @@ class IndexController extends Controller
         	
         	 if($user->isDirty()){
           $user->save();
-          return redirect()->route('staff.index')->with('status', $request->lastname.' updated');
+          return redirect()->route('user.index')->with('status', $request->lastname.' updated');
         }
         
-        return redirect()->route('staff.index')->with('status', 'You did not update any record');
+        return redirect()->route('user.index')->with('status', 'You did not update any record');
     }
 
     /**
@@ -190,9 +178,6 @@ class IndexController extends Controller
         $user = User::find($id);
         
         $role = $request->role;
-        $user->area_id = $request->area;
-        $user->group_id = $request->group;
-        $user->zone_id = $request->zone;
         $user->updated_by = $request->user()->id;
         
        
@@ -202,10 +187,10 @@ class IndexController extends Controller
           DB::table('role_user')->where('user_id', $id)->delete();
           $user->attachRole($role);
           $user->save();
-          return redirect()->route('staff.index')->with('status', 'Role updated');
+          return redirect()->route('user.index')->with('status', 'Role updated');
         }
         
-        return redirect()->route('staff.index')->with('status', 'You did not update any record');
+        return redirect()->route('user.index')->with('status', 'You did not update any record');
         
     }
     
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,6 +10,8 @@
           <div class="card card-primary">
             <div class="card-header">
               <h3 class="card-title">All Users</h3>
+              
+              <a href="{{route('user.create')}}" class="btn btn-info">Add a user<i class="fas fa-edit"></i></a>
 
               <div class="card-tools">
                 <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
@@ -35,84 +37,37 @@
                 </thead>
                 <tbody>
                 <tr>
-                  
-                 
-                  <td>Ijaodola Segun</td>
-                  
-                   
-                  <td>gbagi</td>                                   
+                  <td>fumi bright</td>                   
+                  <td>lekki</td>                                   
                   <td>2347084392212</td>
-                   <td>bimbo@graceinvestement.com</td>
-                  <td>Accountant</td> 
-                  <td><div class="form-group">                        
-                        <select class="form-control">
-                          <option><label>Assigned As</label></option>
-                          <option>Admin</option>
-                          <option>Accountant</option>
-                          <option>Area Officer</option>
-                          <option>Sales Canvasing Officer</option>
-                          <option>Group Supervisor</option>
-                          <option>Zonal Coordinator</option>
-
-                        </select>
-                      </div>
-                     <a href="staff.html" class="btn btn-success"><i class="fas fa-eye"></i></a>
-                        <a href="#" class="btn btn-danger"><i class="fas fa-trash"></i></a>
-
-                      </div></td>                 
-                </tr>
-                 <tr>
+                   <td>fun@grinsy.com</td>
+                  <td>Customer</td> 
+                  <td><a href="" data-toggle="modal" data-target="#modal-" class="btn btn-info">update role<i class="fas fa-edit"></i></a>
+                        
+                         {{csrf_field()}}
+                         {{method_field('DELETE')}}
                  
-                  
-                  <td>Bimbo Stephen</td>
-                                   
-                  <td>Dugbe</td>                                   
-                  <td>2347084392212</td>
-                   <td>bimbo@graceinvestement.com</td>
-                  <td>Sale Canvasing Officer- <a href="view-zone.html">Zone A</a></td> 
-                  <td><div class="form-group">                        
-                        <select class="form-control">
-                          <option><label>Assigned As</label></option>
-                          <option>Admin</option>
-                          <option>Accountant</option>
-                          <option>Area Officer</option>
-                          <option>Sales Canvasing Officer</option>
-                          <option>Group Supervisor</option>
-                          <option>Zonal Coordinator</option>
+                     <a href="staff.html" class="btn btn-success">update user<i class="fas fa-eye"></i></a>
+                        <a href="#" class="btn btn-danger">Delete user<i class="fas fa-trash"></i></a>
 
-                        </select>
-                      </div>
-                     <a href="staff.html" class="btn btn-success"><i class="fas fa-eye"></i></a>
-                        <a href="#" class="btn btn-danger"><i class="fas fa-trash"></i></a>
                       </div></td>                 
                 </tr>
                 <tr>
-                 
-                 
-                  <td>Bayo olatunji</td>
-                                     
-                  <td>Dugbe</td>                                   
+                  <td>sunky tomi</td>                   
+                  <td>lekki</td>                                   
                   <td>2347084392212</td>
-                   <td>bayo@graceinvestement.com</td>
-                  <td>Area Officer- <a href="view-area.html">Area A</a></td> 
-                  <td><div class="form-group">                        
-                        <select class="form-control">
-                          <option><label>Assigned As</label></option>
-                          <option>Admin</option>
-                          <option>Accountant</option>
-                          <option>Area Officer</option>
-                          <option>Sales Canvasing Officer</option>
-                          <option>Group Supervisor</option>
-                          <option>Zonal Coordinator</option>
+                   <td>fun@grinsy.com</td>
+                  <td>Customer</td> 
+                  <td><a href="" data-toggle="modal" data-target="#modal-" class="btn btn-info">update role<i class="fas fa-edit"></i></a>
+                        
+                         {{csrf_field()}}
+                         {{method_field('DELETE')}}
+                 
+                     <a href="staff.html" class="btn btn-success">update user<i class="fas fa-eye"></i></a>
+                        <a href="#" class="btn btn-danger">Delete user<i class="fas fa-trash"></i></a>
 
-                        </select>
-                      </div>
-                      <a href="staff.html" class="btn btn-success"><i class="fas fa-eye"></i></a>
-                        <a href="#" class="btn btn-danger"><i class="fas fa-trash"></i></a>
                       </div></td>                 
                 </tr>
-                                           
-                
                 </tbody>
                 <tfoot>
                 <tr>
